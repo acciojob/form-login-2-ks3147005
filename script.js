e6db8266b90c7ea819060780690c83b446af4290
karan sharma <93695328+ks3147005@users.noreply.github.com>
2024-01-29 19:04:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function submitForm(e) {
+	e.preventDefault();
+
+	var firstName = document.getElementById('firstName').value;
+	var lastName = document.getElementById('lastName').value;
+	var phone = document.getElementById('phone').value;
+	var email = document.getElementById('email').value;
+	
+alert("First Name :" + firstName;
+            "Last Name: " + lastName;
+            "Phone Number: " + phoneNumber;
+            "Email ID: " + email;)
+}    
+             submitForm();          
